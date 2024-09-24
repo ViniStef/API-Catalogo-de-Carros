2eb73b1e1f3d567f1c050240139cd4c0fb4f36c0
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-09-24 13:58:33 -0300

    Added schemas

---
+++ src/schemas/car.schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const carSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string().nullish(),
+  brand: z.string(),
+  year: z.number(),
+  km: z.number(),
+  color: z.string().nullish(),
+});
+
+const carCreateSchema = carSchema.omit({ id: true });
+const carUpdateSchema = carCreateSchema.partial({
+  name: true,
+  brand: true,
+  year: true,
+  km: true,
+});
+
+export { carSchema, carCreateSchema, carUpdateSchema };
